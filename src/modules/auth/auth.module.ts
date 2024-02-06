1ee765240eb70f941dc269be925b4b7fd26508c8
Fleming <flemingsyiemlieh8899@gmail.com>
2024-02-06 15:05:50 +0530

    Simple Login Without Passport

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controller/auth/auth.controller';
 import { AuthService } from './service/auth/auth.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
--- src/modules/auth/controller/auth/auth.controller.ts
+++ src/modules/auth/controller/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from '../../service/auth/auth.service';
+import { loginDto } from '../../dtos/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // @UseGuards('local-auth')
+  @Post('login')
+  login(@Request() req, @Body() payload: loginDto): Promise<any> {
+    return this.authService.validateUserCredential(payload);
+  }
+}
